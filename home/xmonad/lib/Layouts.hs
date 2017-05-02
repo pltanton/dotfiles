20ccc7f6f5ef23819498377546b2341d1f3fa0ed
Plotnikov Anton <plotnikovanton@gmail.com>
2017-05-02 03:39:11 +0300

    Returnth of smart borders

--- home/xmonad/lib/Layouts.hs
+++ home/xmonad/lib/Layouts.hs
@@ -1,25 +1,29 @@
 module Layouts where
+
+-- XMonad imports
 import XMonad
 
 import XMonad.Layout.Named
 import XMonad.Layout.Renamed
 import XMonad.Layout.ResizableTile
-import XMonad.Layout.HintedGrid
+import XMonad.Layout.Grid
 import XMonad.Layout.Spacing
 import XMonad.Layout.LayoutModifier
+import XMonad.Layout.NoBorders
 
 layoutIcon :: String -> String
 layoutIcon name = "^i(/home/anton/.xmonad/icons/" ++ name ++ ".xpm)"
 
-myLayout :: Choose (XMonad.Layout.LayoutModifier.ModifiedLayout XMonad.Layout.Renamed.Rename (XMonad.Layout.LayoutModifier.ModifiedLayout Spacing ResizableTall)) (Choose (XMonad.Layout.LayoutModifier.ModifiedLayout XMonad.Layout.Renamed.Rename (Mirror (XMonad.Layout.LayoutModifier.ModifiedLayout Spacing ResizableTall))) (Choose (XMonad.Layout.LayoutModifier.ModifiedLayout XMonad.Layout.Renamed.Rename (XMonad.Layout.LayoutModifier.ModifiedLayout Spacing Grid)) (XMonad.Layout.LayoutModifier.ModifiedLayout XMonad.Layout.Renamed.Rename (XMonad.Layout.LayoutModifier.ModifiedLayout Spacing Full)))) a
+myLayout :: ModifiedLayout SmartBorder (Choose (ModifiedLayout Rename (ModifiedLayout Spacing ResizableTall)) (Choose (ModifiedLayout Rename (Mirror (ModifiedLayout Spacing ResizableTall))) (Choose (ModifiedLayout Rename (ModifiedLayout Spacing Grid)) (ModifiedLayout Rename (ModifiedLayout Spacing Full))))) a
 myLayout =
+    smartBorders $
     named "T" tiled |||
     named "M" (Mirror tiled) |||
     named "G" grid |||
     named "F" full
   where
     tiled   = spaces $ ResizableTall nmaster delta ratio []
-    grid    = spaces $ Grid False
+    grid    = spaces $ Grid
     spaces  = spacing 4
     full    = spaces Full
     nmaster = 1
