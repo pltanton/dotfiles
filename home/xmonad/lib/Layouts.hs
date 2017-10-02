98b23e1a49febeca13b5b2473498ef73491b2583
Anton.Plotnikov <plotnikovanton@gmail.com>
2017-10-02 20:01:10 +0300

    Tabbed xmonad layout

--- home/xmonad/lib/Layouts.hs
+++ home/xmonad/lib/Layouts.hs
@@ -10,21 +10,24 @@ import XMonad.Layout.Grid
 import XMonad.Layout.Spacing
 import XMonad.Layout.LayoutModifier
 import XMonad.Layout.NoBorders
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Gaps
 
 layoutIcon :: String -> String
 layoutIcon name = "^i(/home/anton/.xmonad/icons/" ++ name ++ ".xpm)"
 
-myLayout :: ModifiedLayout SmartBorder (Choose (ModifiedLayout Rename (ModifiedLayout Spacing ResizableTall)) (Choose (ModifiedLayout Rename (Mirror (ModifiedLayout Spacing ResizableTall))) (Choose (ModifiedLayout Rename (ModifiedLayout Spacing Grid)) (ModifiedLayout Rename (ModifiedLayout Spacing Full))))) a
 myLayout =
     smartBorders $
     named "T" tiled |||
     named "M" (Mirror tiled) |||
     named "G" grid |||
-    named "F" full
+    named "B" tabbed
   where
+    tabbed  = gapsed simpleTabbed
     tiled   = spaces $ ResizableTall nmaster delta ratio []
     grid    = spaces $ Grid
     spaces  = spacing 4
+    gapsed  = gaps $ zip [U, D, R, L] [2..]
     full    = spaces Full
     nmaster = 1
     ratio   = 1/2
