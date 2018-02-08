ab97caf64aa8568b8585019f7654dfbf1320eb57
Plotnikov Anton <plotnikovanton@gmail.com>
2018-02-09 01:19:29 +0300

    New rofi config

--- home/xmonad/lib/Autorun.hs
+++ home/xmonad/lib/Autorun.hs
@@ -6,8 +6,7 @@ import XMonad.Util.SpawnOnce
 autorunHook = do
     -- Uncomment after xmonad 0.14 release and change spawn to spawnOnce
     -- spawnOnce "/scripts/monitor-hotplug.sh"
-    spawn "xautolock -time 10 -locker 'i3lock-next'"
-    spawn "/scripts/wallaper.sh"
+    spawn "feh --bg-fill /home/anton/.xmonad/wall.png"
     spawn "/scripts/kbd.sh -d"
     spawn "/scripts/touchpad.sh -d"
     spawn "xsetroot -cursor_name left_ptr"
@@ -15,5 +14,4 @@ autorunHook = do
     spawn "kbdd"
     spawn "dunst"
     spawnOnce "echo nothing"
-    -- spawnOnce "xssproxy"
 
